607a2e13154a6fd9c47fef05be0979a2806cec8b
Htet Ko Oo <htetkooo2532@gmail.com>
2025-07-26 14:13:15 +0700

    auth middleware fixing

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 // Web Routes
-Route::get('admin/login', 'Admin\AuthController@showLogin');
-Route::post('admin/login', 'Admin\AuthController@login')->name('admin.login');
+Route::prefix('admin')->namespace('Admin')->middleware('guest:admin')->group( function () {
+    Route::get('login', 'AuthController@showLogin')->name('admin.login.form');
+    Route::post('login', 'AuthController@login')->name('admin.login');
+});
+
 
 //admin routes
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'RedirectIfNotAdmin'], function () {
+Route::prefix('admin')->namespace('Admin')->middleware('auth:admin')->group( function () {
+
     Route::get('/', 'PageController@dashboard')->name('admin');
 
     Route::resource('programming', 'ProgrammingController');
